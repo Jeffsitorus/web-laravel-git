7de8c7fa00d9bfab715564499476ea1be0d3d776
Jeff19-web <jefrisitorus22@gmail.com>
2020-06-09 09:58:26 +0700

    Membuat blade templating

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar py-3 navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Web Laravel</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">About</a>
+        <a class="nav-item nav-link" href="#">Contact</a>
+        <a class="nav-item nav-link" href="#">Post</a>
+      </div>
+    </div>
+  </div>
+</nav>
