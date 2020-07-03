10b32190bb82f87ea3c9cda2ffebda6d613a04d6
Jeffsitorus <jefrisitorus22@gmail.com>
2020-07-03 19:23:51 +0700

    Relationship many to many (blog dan tag)

---
+++ database/migrations/2020_06_29_083714_create_blog_tag_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_tag', function (Blueprint $table) {
+            $table->foreignId('blog_id');
+            $table->foreignId('tag_id');
+            $table->primary(['blog_id', 'tag_id']);
+
+            $table->foreign('blog_id')->references('id')->on('blogs')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_tag');
+    }
+}
